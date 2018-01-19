2d21f64359271249c5d57364daebc56fd1844795
keruthika <30829818+keruthika@users.noreply.github.com>
2018-01-19 08:12:09 +0000

    Create array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main()
+{
+int a[5],n,i;
+float sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+sum=sum=a[i];
+}
+return 0;
+}
+
